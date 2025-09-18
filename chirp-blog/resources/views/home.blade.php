0b2547d4f736523892decfc3292f61ce04833f45
zoli87webdev <zoli87webdev@gmail.com>
2025-09-18 20:26:54 +0200

    first commit

---
+++ chirp-blog/resources/views/home.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    @vite('resources/css/app.css', 'resources/js/app.js')
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center my-5">
+                    <h1>Welcome to Chirp Blog</h1>
+                    <p class="lead">Your go-to place for the latest chirps!</p>
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center my-5">
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
