e6dce653d35b6509d4493fdc699af531585215dd
zoli87webdev <zoli87webdev@gmail.com>
2025-09-18 20:51:40 +0200

    basic view

--- chirp-blog/resources/views/home.blade.php
+++ chirp-blog/resources/views/home.blade.php
@@ -23,13 +23,27 @@
     <main>
         <div class="container">
             <div class="row">
-                <div class="col-12 text-center my-5">
+                <div class="card" style="width: 100%;">
+                    <div class="card-header d-flex align-items-center">
+                        <img src=".." class="card-img-top " alt="...">
+                        <span>name</span>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card’s content.</p>
+                        <div>
+                            <button class="btn btn-info">edit</button>
+                            <button class="btn btn-danger">delete</button>
+                        </div>
+                    </div>
+                </div>
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-    </script>
+
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+                    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+                </script>
 </body>
 
 </html>
